cc5c9a3ebcb40cb46cb7b7d3e81e2f2bdc531c60
Rankyn Bass <rankyn@proton.me>
2022-11-12 07:46:53 -0800

    Updated wine-xiv and dxvk to newest versions

--- src/XIVLauncher.Common.Unix/Compatibility/Dxvk.cs
+++ src/XIVLauncher.Common.Unix/Compatibility/Dxvk.cs
@@ -8,8 +8,8 @@ namespace XIVLauncher.Common.Unix.Compatibility;
 
 public static class Dxvk
 {
-    private const string DXVK_DOWNLOAD = "https://github.com/Sporif/dxvk-async/releases/download/1.10.1/dxvk-async-1.10.1.tar.gz";
-    private const string DXVK_NAME = "dxvk-async-1.10.1";
+    private const string DXVK_DOWNLOAD = "https://github.com/Sporif/dxvk-async/releases/download/2.0/dxvk-async-2.0.tar.gz";
+    private const string DXVK_NAME = "dxvk-async-2.0";
 
     public static async Task InstallDxvk(DirectoryInfo prefix, DirectoryInfo installDirectory)
     {
