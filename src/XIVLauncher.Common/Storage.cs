5bfc8c6e81e4cbf170d577ef6030bc2e8269db91
Rankyn Bass <rankyn@proton.me>
2026-02-13 18:17:42 -0800

    Migrate Linux to XDG spec

--- src/XIVLauncher.Common/Storage.cs
+++ src/XIVLauncher.Common/Storage.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Formats.Asn1;
 using System.IO;
 
 namespace XIVLauncher.Common;
@@ -12,6 +13,34 @@ public class Storage
         if (Environment.OSVersion.Platform == PlatformID.Win32NT)
         {
             this.Root = new DirectoryInfo(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), appName));
+        }
+        else if (System.OperatingSystem.IsLinux())
+        {
+            var HOME = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+            var XDG_DATA_HOME = Environment.GetEnvironmentVariable("XDG_DATA_HOME");
+            if (string.IsNullOrEmpty(XDG_DATA_HOME) || !Directory.Exists(XDG_DATA_HOME))
+            {
+                XDG_DATA_HOME = Path.Combine(HOME, ".local", "share");
+            }
+            var dataHomeDir = new DirectoryInfo(Path.Combine(XDG_DATA_HOME, appName));
+            var xlcore = new DirectoryInfo(Path.Combine(HOME, $".{appName}"));
+            if (!dataHomeDir.Exists && !xlcore.Exists)
+            {
+                // If neither exists, create at $XDG_DATA_HOME
+                this.Root = dataHomeDir;
+            }
+            else if (xlcore.Exists && !dataHomeDir.Exists)
+            {
+                // If ~/.xlcore exists and $XDG_DATA_HOME/xlcore doesn't, move ~/.xlcore and create symlink
+                xlcore.MoveTo(dataHomeDir.FullName);
+                Directory.CreateSymbolicLink(xlcore.FullName, dataHomeDir.FullName);
+                this.Root = dataHomeDir;
+            }
+            else
+            {
+                // Otherwise, use $XDG_DATA_HOME/xlcore
+                this.Root = dataHomeDir;
+            }
         }
         else
         {
